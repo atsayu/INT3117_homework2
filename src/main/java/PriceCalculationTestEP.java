72d6d8d54953253df6e2c176810ecdddd810ee1d
unknown <quyenhoangntt03@gmail.com>
2023-09-28 23:53:15 +0700

    Initial commit

---
+++ src/main/java/PriceCalculationTestEP.java
@@ -0,0 +1,7 @@
+public class PriceCalculationTest {
+    public static void main(String[] args) {
+        double d = -Double.MAX_VALUE;
+        System.out.println(d);
+        System.out.println(Double.MAX_VALUE);
+    }
+}
